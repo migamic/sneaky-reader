590c11db4d5b20ed3eb3ff3f5db3a0ed89825565
Jaume Ros Alonso <jaume.ros.alo@gmail.com>
2024-02-24 00:11:54 +0100

    Editor text that appears on keystroke

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Editor from './Editor.jsx'
 export const BookContext = createContext();
 
 function App() {
-  const [text, setText] = useState('Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.')
+  const [text, setText] = useState(["Lorem", "ipsum", "dolor", "sit", "amet,", "consectetur", "adipiscing", "elit.", "\n", "Sed", "do", "eiusmod", "tempor", "incididunt", "ut", "labore", "et", "dolore", "magna", "aliqua.", "\n", "Ut", "enim", "ad", "minim", "veniam,", "quis", "nostrud", "exercitation", "ullamco", "laboris", "nisi", "ut", "aliquip", "ex", "ea", "commodo", "consequat.", "\n", "Duis", "aute", "irure", "dolor", "in", "reprehenderit", "in", "voluptate", "velit", "esse", "cillum", "dolore", "eu", "fugiat", "nulla", "pariatur.", "\n", "Excepteur", "sint", "occaecat", "cupidatat", "non", "proident,", "sunt", "in", "culpa", "qui", "officia", "deserunt", "mollit", "anim", "id", "est", "laborum."])
 
   return (
     <>
--- src/Editor.jsx
+++ src/Editor.jsx
@@ -1,9 +1,31 @@
-const Editor = () => {
+import { useState, useEffect, useContext } from 'react'
+import { BookContext } from './App.jsx'
+
+const Editor = ( {text} ) => {
+  const bookText = useContext(BookContext);
+  const [shownWordsNum, setShownWordsNum] = useState(20);
+
+  useEffect(() => {
+        const handleKeyPress = () => {
+            setShownWordsNum(count => count < bookText.length ? count + 1 : count);
+        };
+
+        window.addEventListener('keydown', handleKeyPress);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyPress);
+        };
+    }, []);
+
+
   return (
-    <p>
-      Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-    </p>
+    <div>
+      {bookText.slice(0, shownWordsNum).map((word, index) => (
+        <span key={index}>{word} </span>
+      ))}
+    </div>
+
   );
 };
 
-export default Editor 
+export default Editor
